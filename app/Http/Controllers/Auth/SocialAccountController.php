6852460ac53eaba9f2775fd8217915ccb7b3b154
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-12-06 20:58:18 -0700

    1.3 - Created controllers and model for storing social accounts

---
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SocialAccountController extends Controller
+{
+  public function redirectToProvider($provider)
+  {
+
+  }
+
+  public function handleProviderCallback()
+  {
+    
+  }
+}
